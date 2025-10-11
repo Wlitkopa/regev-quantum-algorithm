b31422b967a8abbbfaee5b8f6404f900281cf693
Wlitkopa <92793111+Wlitkopa@users.noreply.github.com>
2025-10-11 19:18:43 +0200

    Update regev_all.py

--- regev_all.py
+++ regev_all.py
@@ -36,36 +36,6 @@ find_pq = True
 # Parameter denoting if a decomposed version of quantum circuit should be drawn (drawing quantum circuit)
 decompose = False
 
-
-while True:
-    print("========== REGEV'S ALGORITHM ==========\n")
-
-    print('''Choose an option:
-    1. Run all algorithm
-    2. Run quantum part
-    3. Run classical part
-    4. Draw quantum circuit
-    5. Exit''')
-
-    while True:
-        try:
-            choice = int(input())
-            break
-        except ValueError:
-            print("Please enter a number.")
-            continue
-
-    match choice:
-        case 1:
-            print("------- Running all algorithm -------")
-            regev.run_all_algorithm(Ns, d_qd_list, number_of_combinations, type_of_test, find_pq)
-            print("Finished running all algorithm. The results are saved in output_data/regev/all_parts folder")
-            continue
-
-# Parameter denoting if a decomposed version of quantum circuit should be drawn (drawing quantum circuit)
-decompose = False
-
-
 while True:
     print("========== REGEV'S ALGORITHM ==========\n")
 
